6aae67aa619a2b87c0ee514890f9e1cff4324890
309464462 <309464462@qq.com>
2018-10-25 17:25:22 +0800

    python第一张

---
+++ python/python_essesiontial_exercise/1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 25 16:16:11 2018
+
+@author: Administrator
+"""
+
+#x = input("the meaning of life:")
+#print(x)
+#if 1 == 2 : 
+#    print('hello')
+#else:
+#    print('false')
+#print(2**3)
+#print(pow(2,3))
+#print(abs(-10))
+#print(round(1.0/0.8))
+#import math
+#print(math.floor(32.9))
+#print(math.sqrt(31.9))
+
+#import cmath
+#print(cmath.sqrt(-1))
+
+#print((1+1j)*(9+4j))
+
+name = raw_input('what is your name?')
+print('hello,'+name+'!')
