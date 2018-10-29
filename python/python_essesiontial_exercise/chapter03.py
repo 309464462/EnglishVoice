144a16cae4659d1519ff58616691b9aac949adb6
309464462 <309464462@qq.com>
2018-10-30 00:06:34 +0800

    chapter03

--- python/python_essesiontial_exercise/chapter03.py
+++ python/python_essesiontial_exercise/chapter03.py
@@ -5,3 +5,23 @@ Created on Mon Oct 29 23:40:35 2018
 @author: elvin
 """
 
+#proverb = "a penny saved"
+#proverb = proverb +" is penny earned"
+#print(proverb)
+#pennies_saved = 0
+#pennies_saved += 1
+#print(pennies_saved)
+#a = ("one","two","three")
+#b = (a,"asdfasdfasfd")
+#print(a[len(a)-1])
+#print(b[0][1])
+#print(b[1][2])
+
+#a = {"a":0,"b":1,"c":3,"a":4}
+#print(a)
+#print(a["a"])
+
+b = ["q","2w","e","r"]
+print(b[-1])
+print(b[-2])
+print(b[-3])
