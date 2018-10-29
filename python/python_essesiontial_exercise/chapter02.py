20ef790ecffbf45d9b5bb47d6f30d248d84891c7
309464462 <309464462@qq.com>
2018-10-29 23:41:08 +0800

    add chapter03

--- python/python_essesiontial_exercise/chapter02.py
+++ python/python_essesiontial_exercise/chapter02.py
@@ -4,12 +4,23 @@ Created on Mon Oct 29 12:39:30 2018
 
 @author: Administrator
 """
-
+#print(type())
 #print(type(1))
 #print(type(2000))
 #print(type(99999999))
 #print(type(1.0))
+#print(type(1.01+12j))
 #<class 'int'>
 #<class 'int'>
 #<class 'int'>
 #<class 'float'>
+#<class 'complex'>
+#print("%f"%(4025-2.4))
+#print("%.2f"%(5/3))
+#print("%f"%(5/3))
+#print("%0.f"%(5/3))
+#4022.600000
+#1.67
+#1.666667
+#2
+
---
+++ python/python_essesiontial_exercise/chapter03.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 23:40:35 2018
+
+@author: elvin
+"""
+
