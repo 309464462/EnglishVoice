b2355f9f8a134f9f58bde2b56397d6b6fb296f3e
309464462 <309464462@qq.com>
2018-11-08 23:07:36 +0800

    demo数据

--- python/python_essesiontial_exercise/Chapter 9/Ch9.py
+++ python/python_essesiontial_exercise/Chapter 9/Ch9.py
@@ -1,159 +1,170 @@
-# These examples are not in individual functions in the chapter, but
-# to isolate them, they are separated into individual functions here
+# # These examples are not in individual functions in the chapter, but
+# # to isolate them, they are separated into individual functions here
+# import os
+# def lambda_sample():
+#     # use lambda with filter
+#     filter_me = [1, 2, 3, 4, 6,7 ,8, 11, 12, 14, 15, 19, 22]
+#     # This will only return true for even numbers (because x%2 is 0, or False,
+#     # for odd numbers)
+#     result = filter(lambda x: x%2 == 1, filter_me)
+#     print(*result)
+# lambda_sample()
+#
+#
+# def lambda_named_sample():
+#     # use lambda with filter, but bind it to a name
+#     filter_me = [1, 2, 3, 4, 6,7 ,8, 11, 12, 14, 15, 19, 22]
+#     # This will only return true for even numbers (because x%2 is 0, or False,
+#     # for odd numbers)
+#     func = lambda x: x%2 == 0
+#     result = filter(func, filter_me)
+#     print(*result)
+# lambda_named_sample()
+#
+#
+# def map_sample():
+#     # Now map gets to be run in the simple case
+#     map_me = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]
+#     result = map(lambda x: "The letter is %s\n" % x, map_me)
+#     print(*result)
+# map_sample()
+#
+#
+# def map_sample2():
+#     # use map with a list of lists, to re-order the output.
+#     map_me_again = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#     result = map(lambda list: [ list[1], list[0], list[2]], map_me_again)
+#     print(*result)
+# map_sample2()
+#
+#
+# def list_comprehension_sample():
+#     # First, just print even numbers
+#     everything = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+#     print([ "odd data:%d "%(x) for x in everything if x%2 == 0 and x%3 == 0])
+# list_comprehension_sample()
+#
+# def range_sample():
+#     # First an example of a range between two numbers
+#     f = range (10, 20)
+#     print(*f)
+#
+#
+#     # range works differently with only one parameter
+#     for number in range(10):
+#         print("Number is now %d" % number)
+#
+#     # Using a stride
+#     for number in range(5, 55, 4):
+#         print("Number from 5 to 55, by fours: %d" % number)
+#
+# range_sample()
+# def string_substitution_sample():
+#     person = {"name": "James", "camera": "nikon", "handedness": "lefty", "baseball_team": "angels", "instrument": "guitar"}
+#
+#     print("%(name)s, %(camera)s, %(baseball_team)s" % person)
+#
+#     person["height"] = 1.6
+#     person["weight"] = 80
+#     print("%(name)s, %(camera)s, %(baseball_team)s, %(height)2.2f, %(weight)2.2f" % person)
+#
+#     # An alternative way to perform these substitutions
+#     import string
+#     person = {"name": "James", "camera": "nikon", "handedness": "lefty", "baseball_team": "angels", "instrument": "guitar"}
+#     person["height"] = 1.6
+#     person["weight"] = 80
+#     t = string.Template("$name is $height m high and $weight kilos")
+#     print(t.substitute(person))
+# string_substitution_sample()
+# #
+#
+# def getopt_sample():
+#     import sys
+#     import getopt
+#     # Remember, the first thing in the sys.argv list is the name of the command
+#     # You don't need that.
+#     cmdline_params = sys.argv[1:]
+#
+#     opts, args = getopt.getopt(cmdline_params, 'hc:', ['help', 'config='])
+#
+#
+#     for option, parameter in opts:
+#
+#         if option == '-h' or option == '--help':
+#             print("This program can be run with either -h or --help for this message,")
+#             print("or with -c or --config=<file> to specify a different configuration file")
+#         if option in ('-c', '--config'): # this means the same as the above
+#             print("Using configuration file %s" % parameter)
+#
+# getopt_sample()
+# def gnu_getopt_sample():
+#     import sys
+#     import getopt
+#     # Remember, the first thing in the sys.argv list is the name of the command
+#     # You don't need that.
+#     cmdline_params = sys.argv[1:]
+#
+#     opts, args = getopt.gnu_getopt(cmdline_params, 'hc:', ['help', 'config='])
+#
+#     for option, parameter in opts:
+#
+#         if option == '-h' or option == '--help':
+#             print("This program can be run with either -h or --help for this message,")
+#             print("or with -c or --config=<file> to specify a different configuration file")
+#
+#         if option in ('-c', '--config'): # this means the same as the above
+#             print("Using configuration file %s" % parameter)
+#
+# def fork_sample():
+#     import os
+#     pid = os.fork()
+#     # fork and exec together
+#     print("second test")
+#     if pid == 0: # This is the child
+#         print("this is the child")
+#         print("I'm going to exec another program now")
+#         os.execl('dir')
+#     else:
+#         print("the child is pid %d" % pid)
+#         os.wait()
+#
+# fork_sample()
+# def determine_platform_sample():
+#     import os, sys
+#     if sys.platform == 'win32':
+#         print("Running on a windows platform")
+#         command = "C:\\Windows\\system32\\cmd.exe"
+#         params = ["ipconfig"]
+#
+#     if sys.platform == 'linux2':
+#         print("Running on a Linux system, identified by %s" % sys.platform)
+#         command = '/bin/uname'
+#         params = ['uname', '-a']
+#
+#     print("Running %s" % command)
+#     os.spawnv(os.P_WAIT, command, params)
+# determine_platform_sample()
 
-def lambda_sample():
-    # use lambda with filter
-    filter_me = [1, 2, 3, 4, 6,7 ,8, 11, 12, 14, 15, 19, 22]
-    # This will only return true for even numbers (because x%2 is 0, or False,
-    # for odd numbers)
-    result = filter(lambda x: x%2 == 0, filter_me)
-    print(*result)
-
-
-def lambda_named_sample():
-    # use lambda with filter, but bind it to a name
-    filter_me = [1, 2, 3, 4, 6,7 ,8, 11, 12, 14, 15, 19, 22]
-    # This will only return true for even numbers (because x%2 is 0, or False,
-    # for odd numbers)
-    func = lambda x: x%2 == 0
-    result = filter(func, filter_me)
-    print(*result)
-
-
-def map_sample():
-    # Now map gets to be run in the simple case
-    map_me = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]
-    result = map(lambda x: "The letter is %s" % x, map_me)
-    print(*result)
-
-def map_sample2():
-# use map with a list of lists, to re-order the output.
-map_me_again = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-result = map(lambda list: [ list[1], list[0], list[2]], map_me_again)
-print(*result)
-
-
-def list_comprehension_sample():
-    # First, just print even numbers
-    everything = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
-    print([ x for x in everything if x%2 == 0 ])
-
-def range_sample():
-    # First an example of a range between two numbers
-    f = range (10, 20)
-    print(*f)
-
-    # range works differently with only one parameter
-    for number in range(10):
-        print("Number is now %d" % number)
-
-    # Using a stride
-    for number in range(5, 55, 4):
-        print("Number from 5 to 55, by fours: %d" % number)
-
-
-def string_substitution_sample():
-    person = {"name": "James", "camera": "nikon", "handedness": "lefty", "baseball_team": "angels", "instrument": "guitar"}
-
-    print("%(name)s, %(camera)s, %(baseball_team)s" % person)
-
-    person["height"] = 1.6
-    person["weight"] = 80
-    print("%(name)s, %(camera)s, %(baseball_team)s, %(height)2.2f, %(weight)2.2f" % person)    
-
-    # An alternative way to perform these substitutions
-    import string
-    person = {"name": "James", "camera": "nikon", "handedness": "lefty", "baseball_team": "angels", "instrument": "guitar"}
-    person["height"] = 1.6
-    person["weight"] = 80
-    t = string.Template("$name is $height m high and $weight kilos")
-    print(t.substitute(person))
-
-
-def getopt_sample():
-    import sys
-    import getopt
-    # Remember, the first thing in the sys.argv list is the name of the command
-    # You don't need that.
-    cmdline_params = sys.argv[1:]
-
-    opts, args = getopt.getopt(cmdline_params, 'hc:', ['help', 'config='])
-    
-
-    for option, parameter in opts:
-   
-        if option == '-h' or option == '--help':
-            print("This program can be run with either -h or --help for this message,")
-            print("or with -c or --config=<file> to specify a different configuration file")
-        if option in ('-c', '--config'): # this means the same as the above
-            print("Using configuration file %s" % parameter)
-
-def gnu_getopt_sample():
-    import sys
-    import getopt
-    # Remember, the first thing in the sys.argv list is the name of the command
-    # You don't need that.
-    cmdline_params = sys.argv[1:]
-
-    opts, args = getopt.gnu_getopt(cmdline_params, 'hc:', ['help', 'config='])
-    
-    for option, parameter in opts:
-   
-        if option == '-h' or option == '--help':
-            print("This program can be run with either -h or --help for this message,")
-            print("or with -c or --config=<file> to specify a different configuration file")
-        
-        if option in ('-c', '--config'): # this means the same as the above
-            print("Using configuration file %s" % parameter)
-
-def fork_sample():
-    import os
-    pid = os.fork()
-    # fork and exec together
-    print("second test")
-    if pid == 0: # This is the child
-        print("this is the child")
-        print("I'm going to exec another program now")
-        os.execl('/bin/cat', 'cat', '/etc/motd')
-    else:
-        print("the child is pid %d" % pid)
-        os.wait()        
-
-def determine_platform_sample():
-    import os, sys
-    if sys.platform == 'win32':
-        print("Running on a windows platform")
-        command = "C:\\winnt\\system32\\cmd.exe"
-        params = []
-    
-    if sys.platform == 'linux2':
-        print("Running on a Linux system, identified by %s" % sys.platform)
-        command = '/bin/uname'
-        params = ['uname', '-a']
-
-    print("Running %s" % command)
-    os.spawnv(os.P_WAIT, command, params)
-
-
-def os_system_sample():
-    # Now system
-    if sys.platform == 'win32':
-        print("Running on a windows platform")
-        command = "cmd.exe"
-
-    if sys.platform == 'linux2':
-        print("Running Linux")
-        command = "uname -a"
-
-    os.system(command)
-
-
-def threading_sample():
-#Using sub-classing
+# def os_system_sample():
+#     # Now system
+#     import sys
+#     import os
+#     if sys.platform == 'win32':
+#         print("Running on a windows platform")
+#         command = "cmd.exe"
+#
+#     if sys.platform == 'linux2':
+#         print("Running Linux")
+#         command = "uname -a"
+#     os.system(command)
+#
+# os_system_sample()
+# # def threading_sample():
+# #Using sub-classing
 import math
 from threading import Thread
 import time
-
+#
 class SquareRootCalculator:
 
     """This class spawns a separate thread to calculate a bunch of square
@@ -171,7 +182,7 @@ class SquareRootCalculator:
             time.sleep(1)
         print("Calculated %s square root(s); the last one is sqrt(%d)=%f" %
               (target, len(self.results), self.results[-1]))
-              
+
     class CalculatorThread(Thread):
         """A separate thread which actually does the calculations."""
 
@@ -198,9 +209,9 @@ if __name__ == '__main__':
         try:
             limit = int(limit)
         except ValueError:
-            print("Usage: %s [number of square roots to calculate]" 
-                  % sys.argv[0])  
+            print("Usage: %s [number of square roots to calculate]"
+                  % sys.argv[0])
     SquareRootCalculator(limit)
-
-
-    
+#
+#
+#
