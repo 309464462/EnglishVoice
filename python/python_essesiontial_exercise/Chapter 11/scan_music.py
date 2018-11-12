0a5311c3df1557227b4ebee74dce162f0ca4ca2c
309464462 <309464462@qq.com>
2018-11-13 00:47:46 +0800

    去掉有空格的

--- python/python_essesiontial_exercise/Chapter 11/scan_music.py
+++ python/python_essesiontial_exercise/Chapter 11/scan_music.py
@@ -6,8 +6,10 @@ def print_music(root,dir,files):
     for file in files:
         path = os.path.join(root,file)
         path = os.path.normcase(path)
-        if re.search(r".*\.flac",path):
-            print(path)
+        if not re.search(r".*\.flac",path):continue
+        if re.search(r" ",path):continue
+
+        print(path)
 
 for root, dirs, files in os.walk('F:\\'):
     print_music(root, dirs, files)
