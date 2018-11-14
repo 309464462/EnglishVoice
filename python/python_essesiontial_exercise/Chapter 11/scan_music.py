5eb623ba8cdf7c6429c8be5047f0c89937df5436
309464462 <309464462@qq.com>
2018-11-14 23:31:02 +0800

    add chapter11

--- python/python_essesiontial_exercise/Chapter 11/scan_music.py
+++ python/python_essesiontial_exercise/Chapter 11/scan_music.py
@@ -2,14 +2,15 @@ import  os
 import re
 # print(os.getcwd())
 # print(os.state('.'))
-def print_music(root,dir,files):
-    for file in files:
-        path = os.path.join(root,file)
-        path = os.path.normcase(path)
-        if not re.search(r".*\.flac",path):continue
-        if re.search(r" ",path):continue
-
-        print(path)
-
-for root, dirs, files in os.walk('F:\\'):
-    print_music(root, dirs, files)
+# def print_music(root,dir,files):
+#     for file in files:
+#         path = os.path.join(root,file)
+#         path = os.path.normcase(path)
+#         if not re.search(r".*\.flac",path):continue
+#         if re.search(r" ",path):continue
+#
+#         print(path)
+#
+# for root, dirs, files in os.walk('F:\\'):
+#     print_music(root, dirs, files)
+print(os.stat(os.getcwd()))
